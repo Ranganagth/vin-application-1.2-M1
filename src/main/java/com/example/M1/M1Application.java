6616f61312eaae6c2ccd0a22cae3c5b4f43f11a1
Ranganath <87424392+Ranganagth@users.noreply.github.com>
2022-05-18 17:57:18 +0530

    Update M1Application.java
    Second commit

--- src/main/java/com/example/M1/M1Application.java
+++ src/main/java/com/example/M1/M1Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class M1Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(M1Application.class, args);
 	}
